a3aa83f53f8e0038837162961c8ecfe35cc8f462
Josef Vojkovský <pepavoj@email.cz>
2021-10-27 18:13:21 +0200

    sestiuhelnik

--- du1/du1_josef_vojkovsky.py
+++ du1/du1_josef_vojkovsky.py
@@ -2,12 +2,14 @@ from turtle import *
 # základní informace pro hráče
 print("""
 Dobrý den, 
-budete hrát piškvorky v poli 3x3. Musíte sledovat jak hrací tabulku, tak dodatečné informace v terminálu.
+budete hrát piškvorky v šestiúhelníkovém poli 3x3. Musíte sledovat jak hrací tabulku, tak dodatečné informace v terminálu.
 Během hry může kdykoliv napsat quit a tím hru ukončit.
-Rozmístění políček je jako na numerické klávesnici, tedy takto:
-7  8  9
-4  5  6
-1  2  3
+Rozmístění políček je následující:
+         3 
+     2       6
+ 1       5       9
+     4       8
+         7
 """)
 # jména hráčů pro oslovení během hry
 hrac_1 = input("Jméno hráče 1: ")
@@ -17,20 +19,32 @@ print("Váš tvar jsou kolečka.")
 # posun doleva dolů, aby bylo dost místa + vykreslení tabulky 3x3
 speed(0)
 up()
-setpos(-250,-150)
+left(180)
+forward(300)
+left(180)
 down()
-hrana=100
-for _ in range (3):
+for _ in range(3):
     for _ in range(3):
-        for _ in range(4):
-            forward(hrana)
-            left(90)
-        forward(hrana)
-    left(180)
-    forward(hrana*3)
-    right(90)
-    forward(hrana)
-    right(90)
+        for _ in range(6):
+            forward(50)
+            left(60)
+        forward(50)
+        left(60)
+        forward(50)
+        right(60)
+    right(120)
+    for _ in range(3):
+            forward(50)
+            right(60)
+            forward(50)
+            left(60)
+    left(120)
+    forward(50)
+    right(60)
+    up()
+    forward(50)
+    left(60)
+    down()
 # hra samotná - celý for cyklus
 speed(6)
 for kolo in range(9):
@@ -44,38 +58,37 @@ for kolo in range(9):
     while tah < 1 or tah > 9:
         tah = int(input("zkus to znovu: "))
     if tah == 1:
-        setpos(-200, -140)
+        setpos(-275, 8)
     elif tah == 2:
-        setpos(-100,-140)
+        setpos(-200,51)
     elif tah == 3:
-        setpos(0,-140)         
+        setpos(-125,94)         
     elif tah == 4:
-        setpos(-200, -40)
+        setpos(-200,-35)
     elif tah == 5:
-        setpos(-100,-40)
+        setpos(-125,8)
     elif tah == 6:
-        setpos(0,-40)         
+        setpos(-50, 51)         
     elif tah == 7:
-        setpos(-200, 60)
+        setpos(-125, -78)
     elif tah == 8:
-        setpos(-100, 60)     
+        setpos(-50, -35)     
     elif tah == 9:
-        setpos(0,60)
+        setpos(25,8)
 # jak se pozná, jestli se kreslí křížek nebo kolečko (jako u oslovení) + vykreslení tvaru
     if kolo % 2 == 0:
         left(90)
-        forward(40)
+        forward(35)
         left(45)
         down()
         for _ in range(4):
-            forward(60)
+            forward(40)
             left(180)
-            forward(60)
+            forward(40)
             left(90)
         right(135)
     else:
         down()
-        circle(40)
+        circle(35)
 print("A jsme u konce, klikněte do okna piškvorek pro ukončení.")
 exitonclick()
-    
